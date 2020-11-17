945bd19aff6ad42263bfb27ea8975787b9a23432
Yasinkaya2020 <74434905+Yasinkaya2020@users.noreply.github.com>
2020-11-17 22:14:24 +0300

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -1,8 +1,9 @@
 
 public class BirinciGun {
 	public static void main(String[] args) {
-		System.out.println("11:57 - Birinci G�n");
-		System.out.println("�kinci Commit");
+		System.out.println("11:57 - Birinci Gün");
+		System.out.println("İkinci Commit");
+		system.out.println("10:11 - İkinci Gün");
 	}
 
 }
