251b8465b84f07d89fa1c8e0a4dbc1d7b92b900f
Yasinkaya2020 <74434905+Yasinkaya2020@users.noreply.github.com>
2020-11-17 23:29:31 +0300

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -9,6 +9,7 @@ public class BirinciGun {
 		System.out.println("10:35 - İkinci gün");
 		System.out.println("11:05 - local");
 		System.out.println("11:23 - github.com");
+		System.out.println("11:28 - bakalim conflict olacakmi");
 
 	}
 
