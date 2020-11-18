bb4b5474156ca2e1f6d6a7f6c2e00af95da68ea8
YasinKaya <polenyahya@gmail.com>
2020-11-18 23:13:38 +0300

    mesaj

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -12,7 +12,8 @@ public class BirinciGun {
 		System.out.println("ben buradan conflict beklemiyorum...");
 		System.out.println("yepyeni bir satir");
 		System.out.println("10:26  - feature branch");
-		
+		System.out.println("11:09 - master branch");
+
 
 
 
